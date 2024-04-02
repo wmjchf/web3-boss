2a5bb17fe6b7b43fce19dad098f31f7070147d00
wmjchf <3284304056@qq.com>
2024-04-02 16:42:52 +0800

    feat: 获取当前用户信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { Toaster } from "react-hot-toast";
 import { config } from "./wagmi/config";
 import router from "./router";
+import { getCurrentUser } from "./api/user";
 
 const queryClient = new QueryClient();
 export const App = () => {
+  const handleGetCurrentUser = async () => {
+    const result = await getCurrentUser();
+    console.log(result, "fsfds");
+  };
+  useEffect(() => {
+    // handleGetCurrentUser();
+  }, []);
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,8 +1,16 @@
 import { post, get } from "@/request";
 
+interface IUser {
+  address: string;
+  chainId: number;
+  integral: number;
+  id: number;
+}
 // export const setProfile = (data: unknown) => post("/api/profile", data);
 
 export const getNonce = () => get<{ nonce: string }>("user/nonce");
 
 export const login = (data: unknown) =>
   post<{ token: string }>("user/login", data);
+
+export const getCurrentUser = () => get<IUser>("/user");
